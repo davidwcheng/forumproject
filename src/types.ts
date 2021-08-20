8668a2d554fac176551489748df3b0c29be73e6a
davidwcheng <cheng.1465@osu.edu>
2021-08-19 22:35:43 -0400

    added Session interface to mycontext

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,16 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 import {Request, Response} from "express";
-import { Session, SessionData } from "express-session";
+import { Session } from "express-session";
 
+declare module "express-session"{
+    interface Session{
+        userId:number
+    }
+}
 export type MyContext = {
     em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
     req: Request & {
-        session: Session & Partial<SessionData> & { userId?: number };
+         session: Session //& Partial<SessionData> & { userId?: number };
     };
     res: Response;
 }
